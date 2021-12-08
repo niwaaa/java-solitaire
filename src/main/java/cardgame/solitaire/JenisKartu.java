97f21e85f7fbc0fa4b590c9d2be602c4ebfbe074
Kurniawan <kurniawanronaldi@gmail.com>
2021-12-08 15:45:00 +0700

    solitaire v1.0.0

---
+++ src/main/java/cardgame/solitaire/JenisKartu.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame.solitaire;
+
+/**
+ *
+ * @author Niwa / Kurniawan - 205314148
+ */
+public enum JenisKartu {
+    WARU, HATI, WAJIK, KERITING;
+}
